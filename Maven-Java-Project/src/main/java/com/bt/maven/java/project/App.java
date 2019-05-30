e8628830715016f132c57121442e41d996db6ab1
Cloud User <ec2-user@ip-172-31-21-98.ap-south-1.compute.internal>
2019-05-30 16:14:46 +0000

    project

---
+++ Maven-Java-Project/src/main/java/com/bt/maven/java/project/App.java
@@ -0,0 +1,18 @@
+package com.bt.maven.java.project;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello jagan!");
+    }
+    public void main1(  )
+    {
+        System.out.println("Hello jagan!");
+    }
+}
